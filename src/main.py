d9f2ed126e33fce633c6ef1b4b5795f57542368b
tommygonzaleza <tommygonzaleza@users.noreply.github.com>
2020-03-03 16:55:10 -0400

    Changes of main.py and models.py added

--- src/main.py
+++ src/main.py
@@ -2,13 +2,14 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+import json
+from flask import Flask, request, jsonify, url_for, make_response
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from models import db
-#from models import Person
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -28,14 +29,241 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
-    response_body = {
-        "hello": "world"
+@app.route('/casacadena/submit_register/', methods=['POST', 'GET'])
+def handle_submit_register():
+    email_from_register_form = request.form['register_email']
+    username_from_register_form = request.form['register_username']
+    cedula_from_register_form = request.form['register_document_id']
+    password_from_register_form = request.form['register_password']
+    city_from_register_form = request.form['register_city']
+    headers = {
+        "Content-Type": "application/json"
     }
+    # check if user exists.
+    requesting_user = User.query.filter_by(email=email_from_register_form).one_or_none()
+
+    # user is requesting todos or user creation and sample todo.
+    if request.method == "POST":
+        print("hello, working!")
+        if len(requesting_user) > 0:
+            # user exists, this is a no go...
+            response_body = {
+                "status": "HTTP_400_BAD_REQUEST. User cannot be created again..."
+            }
+            status_code = 400
+
+        else:
+            # user does not exist, creating succesfully
+            document_id_from_register_form = request.form['register_document_id']
+            name_from_register_form = request.form['register_name']
+            name2_from_register_form = request.form['register_name2']
+            last_name_from_register_form = request.form['register_last_name']
+            last_name2_from_register_form = request.form['register_last']
+            username_from_register_form = request.form['register_username']
+            password_from_register_form = request.form['register_password']
+            phone_number_from_register_form = request.form['register_phone_number']
+            city_from_register_form = request.form['register_city']
+
+
+            print("creating user with this username")
+            new_user = User(username)
+            db.session.add(new_user)
+           
+            
+            
+            db.session.commit()
+
+            response_body = {
+                "status": "HTTP_201_OK. Ok"
+            }
+            status_code = 201
+
+
+    return jsonify(response_body), status_code
+
+@app.route('/casacadena/submit_signin', methods=['POST', 'GET'])
+def handle_submit_signin():
+    email_from_form = request.form['signin_email']
+    headers = {
+        "Content-Type": "application/json"
+    }
+    # check if user exists.
+    requesting_email = User.query.filter_by(email=email_from_form).one_or_none()
+    
+    if len(requesting_email) > 0:
+        password_from_signin_form = request.form['signin_password']
+        if password_from_signin_form == requesting_email.password:
+            response_body = {
+                'id':requesting_email.id,
+                'username':requesting_email.username,
+                'email':requesting_email.email,
+                'authentic':true
+            }
+            status_code = 200
+        else:
+            response_body = {
+                "status": "HTTP_400_BAD_REQUEST. Username and password doesn't match..."
+            }
+            status_code = 400
+
+    return jsonify(response_body), status_code
+
+    # user is requesting todos or user creation and sample todo.
+    if request.method == "POST":
+        print("hello, working!")
+        if len() > 0:
+            # user exists and password is correct, send user id...
+            response_body = {
+                "status": "HTTP_400_BAD_REQUEST. User cannot be created again..."
+            }
+            status_code = 400
+
+        else:
+              # user does not exist, creating succesfully
+            
+            print("creating user with this username")
+            new_user = User(username)
+            db.session.add(new_user)
+            
+            
+            db.session.commit()
+            response_body = {
+                "status": "HTTP_200_OK. Ok"
+            }
+            status_code = 200
+
+    
 
     return jsonify(response_body), 200
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Todo endpoint!
+@app.route("/todos/<username>", methods=["GET", "POST", "PUT", "DELETE"])
+def handle_todos(username):
+    headers = {
+        "Content-Type": "application/json"
+    }
+    # check if user exists.
+    requesting_user = User.query.filter_by(username=username).all()
+    # user is requesting todos or user creation and sample todo.
+    if request.method == "GET":
+        print("hello, working!")
+        if len(requesting_user) > 0:
+            # user exists, this is a no go...
+            response_body = {
+                "status": "HTTP_400_BAD_REQUEST. User cannot be created again..."
+            }
+            status_code = 400
+
+        else:
+            # user does not exist, creating succesfully
+            
+            print("creating user with this username")
+            new_user = User(username)
+            db.session.add(new_user)
+            
+            
+            db.session.commit()
+            response_body = {
+                "status": "HTTP_200_OK. Ok"
+            }
+            status_code = 200
+
+    # user to be created, check if exists first...
+    elif request.method == "POST":
+        print("creating user with sample task")
+
+        if len(requesting_user) > 0:
+            # user exists, this is a no go...
+            response_body = {
+                "status": "HTTP_400_BAD_REQUEST. User cannot be created again..."
+            }
+            status_code = 400
+
+        else:
+            # username not in use, create user with sample task...
+            print("creating user with this username and a sample task")
+            new_user = User(username)
+            db.session.add(new_user)
+            sample_todo = Todo("sample task", username)
+            db.session.add(sample_todo)
+            db.session.commit()
+            response_body = {
+                "status": "HTTP_200_OK. Ok"
+            }
+            status_code = 200
+
+    elif request.method == "PUT":
+        # user wants to refresh full list, check if user exists first...
+        print(f"updating full list for {username}")
+
+        if len(requesting_user) > 0:
+            # user exists, updating whole list...
+            # delete current user tasks...
+            Todo.query.filter_by(user_username=username).delete()
+            new_tasks = json.loads(request.data)
+            
+            for task in new_tasks:
+                # task["user_username"] = username
+                new_task = Todo(task["label"], username)
+                db.session.add(new_task)
+
+            db.session.commit()
+            result = f"A list with {len(new_tasks)} todos was succesfully saved"
+            response_body = {
+                "result": result,
+                "status": "HTTP_200_OK."
+            }
+            status_code = 200
+
+        else: 
+            # user does not exist, this is a no go...
+            response_body = {
+                "status": "HTTP_400_BAD_REQUEST. Cannot update task's list for non existing user..."
+            }
+            status_code = 400
+    
+    elif request.method == "DELETE":
+        # user wants to delete his list and user registry...
+        if len(requesting_user) > 0:
+            # user exists, delete records...
+            # delete current user tasks...
+            Todo.query.filter_by(user_username=username).delete()
+            User.query.filter_by(username=username).delete()
+            db.session.commit()
+            response_body = {
+                "result": "ok",
+                "status": "HTTP_204_NO_CONTENT. User and tasks deleted."
+            }
+            status_code = 204
+        
+        else:
+            # user does not exist, this is a no go...
+            response_body = {
+                "status": "HTTP_400_BAD_REQUEST. Cannot delete a non existing user..."
+            }
+            status_code = 400
+
+    else:
+        response_body = "method is not ready for testing yet"
+        status_code = 501
+
+    return make_response(
+        jsonify(response_body),
+        status_code,
+        headers
+    )
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,16 +2,102 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+class User(db.Model):
+    """ todo list user """
+    user_id= db.Column(db.Integer, primary_key=True)
+    document_id = db.Column(db.String(30), nullable=False)
+    name = db.Column(db.String(30), nullable=False)
+    name2 = db.Column(db.String(30), nullable=True)
+    last_name = db.Column(db.String(30), nullable=False)
+    last_name2 = db.Column(db.String(30), nullable=False)
+    username = db.Column(db.String(30), nullable=False)
+    ip_address = db.Column(db.String(24), nullable=False)
+    email = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(30), nullable=False)
+    phone_number = db.Column(db.String(11), nullable=False)
+    city = db.Column(db.String(30), nullable=False)
+    verified = db.Column(db.Boolean, nullable=False)
+    ping = db.Column(db.String(4), nullable=False)
+    ads = db.relationship('Ads', backref='user', lazy=True)
+    bank = db.relationship('Bank', backref='user', lazy=True)
 
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
+    def __init__(self, username):
+        self.username = username.strip()
 
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
+class Ad(db.Model):
+    """ users to do tasks """
+    ad_id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(30), nullable=False)
+    buyorsell = db.Column(db.Boolean, nullable=False)
+    description = db.Column(db.String(300), nullable=False)
+    min_limit = db.Column(db.Integer, nullable=False)
+    max_limit = db.Column(db.Integer, nullable=False)
+    state = db.Column(db.String(10), nullable=False)
+    rate = db.Column(db.String(10), nullable=False)
+    limit = db.Column(db.Integer, nullable=False)
+    picture = db.Column(db.String(10), nullable=False)
+    isavailable = db.Column(db.Boolean, nullable=False)
+    ad_amount = db.Column(db.Integer, nullable=False)
+    bank_ad = db.relationship('Ad_bank', backref='ad', lazy=True)
+    user_id1 = db.Column(db.Integer, db.ForeignKey('user.user_id'),
+        nullable=False)
+    user_id2 = db.Column(db.Integer, db.ForeignKey('user.user_id'),
+    nullable=False)
+
+    def __init__(self, label, user_username):
+        self.label = label.strip()
+        self.user_username = user_username.strip()
+
+    def serialize(self):
+        return {
+            "label": self.label,
+            "done": self.done
+        } 
+
+class AdBank(db.Model):
+    adbank_id = db.Column(db.Integer, primary_key=True)
+    ad_id = db.Column(db.Integer, db.ForeignKey('ad.ad_id'),
+    nullable=False)
+    bank_id = db.Column(db.Integer, db.ForeignKey('bank.bank_id'),
+    nullable=False)
+
+    # def serialize(self):
+    #     return {
+    #         "label": self.label,
+    #         "done": self.done
+    #     } 
+
+class Bank(db.Model):
+    """ users to do tasks """
+    bank_id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(30), nullable=False)
+    bank_ad = db.relationship('Ad_bank', backref='bank', lazy=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'),
+        nullable=False)
+
+    def __init__(self, label, nombre):
+        
+        self.nombre = nombre.strip()
+
+    # def serialize(self):
+    #     return {
+    #         "label": self.label,
+    #         "done": self.done
+    #     } 
+
+class BankAccount(db.Model):
+    """ users to do tasks """
+    bankaccount_id = db.Column(db.Integer, primary_key=True)
+    account_number = db.Column(db.String(20), nullable=False, default="sample taks")
+    name = db.Column(db.Boolean, default=False)
+    
+
+    def __init__(self, label, user_username):
+        self.label = label.strip()
+        self.user_username = user_username.strip()
+
+    #  def serialize(self):
+    #     return {
+    #         "label": self.label,
+    #         "done": self.done
+    #     }         
